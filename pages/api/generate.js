534e754187543c626364403c16e9f9d1e55f2d84
Uchral Davaajargal <uchral24@gmail.com>
2023-04-12 00:10:50 +0900

    basketball reminder

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -3,7 +3,11 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const googleCalendarApiKey = process.env.GOOGLE_CALENDAR_API_KEY
+const calendarId = process.env.GOOGLE_CALENDAR_ID;
 const openai = new OpenAIApi(configuration);
+const { google } = require('googleapis');
+const calendar = google.calendar('v3');
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
@@ -14,24 +18,16 @@ export default async function (req, res) {
     });
     return;
   }
+  const eventText = await getCalendarEvents();
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
-
+  const character = req.body.character;
+  // res.status(200).json({result: eventText + character})
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: generatePrompt(eventText,character),
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: completion.data.choices[0].message.content });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -48,15 +44,47 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(eventText, character) {
+  return [
+    {"role": "system", "content": `You are a helpful assistant for basketball group chat s reminder. You should act as you are a ${character}.`},
+    {"role": "user", "content": `Please tell me next schedule of basketball session. Here is the info. ${eventText}. Tell us bullet points and use emojis.`},
+]
+}
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+async function getCalendarEvents() {
+  let ret = "";
+  try {
+    const now = new Date().toISOString();
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + 60);
+    const endDateString = endDate.toISOString();
+
+    const response = await calendar.events.list({
+      calendarId: calendarId,
+      timeMin: now,
+      timeMax: endDateString,
+      singleEvents: true,
+      orderBy: 'startTime',
+      key: googleCalendarApiKey,
+    });
+
+    const events = response.data.items;
+
+    if (events.length === 0) {
+      console.log('No upcoming events found.');
+      ret += `no upcoming events`
+      return ret
+    } else {
+      for (let i = 0; i < events.length; i++) {
+        const event = events[i];
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+        ret += `${start} - ${event.summary}\n`;
+      }
+    }
+    return ret
+  } catch (error) {
+    console.error('Error retrieving events', error);
+    return `got error retrieving events`
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
-
   async function onSubmit(event) {
     event.preventDefault();
     try {
@@ -14,14 +13,13 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ character: animalInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
       setResult(data.result);
       setAnimalInput("");
     } catch(error) {
@@ -30,6 +28,21 @@ export default function Home() {
       alert(error.message);
     }
   }
+
+  useEffect(() => {
+    // resultが空の場合は、linesに空の配列を設定する
+    let lines
+    if(typeof result !== 'string'){
+      lines = result
+    }
+    else lines = result ? result.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    )) : [];
+    setResult(lines);
+  }, [result]);
 
   return (
     <div>
@@ -40,17 +53,20 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Enter character prompt</h3>
+        <p> ex: LeBron James motivative way, Tsundere, Eren Yeager</p>
+        <p>after push generate button wait for 15 seconds</p>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter an character prompt"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate response" />
         </form>
+        <p>coming soon: form of calendar ID(automaticaly generating katstushika zaal calendar)</p>
         <div className={styles.result}>{result}</div>
       </main>
     </div>
